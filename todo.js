2ef7cd260dd503fc17c40d2115304d49a7086ad3
song2chan <81168390+song2chan@users.noreply.github.com>
2021-04-27 22:31:16 +0900

    0427 todo patch

--- todo.js
+++ todo.js
@@ -4,15 +4,32 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function saveToDos() {
+  for (var i = 0; i < toDos.length; i++) {
+    toDos[i].id
+  }
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.InnerHTML = "X";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "X";
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,9 +40,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
-    // ul에 하나씩 넣어줌.
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    })
   }
 }
 
